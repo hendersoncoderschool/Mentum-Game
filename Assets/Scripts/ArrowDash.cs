aa546eec55848b769d04d6ba8d58e04c59cc12f7
bowser-bash <garrisong2009@gmail.com>
2026-03-14 14:53:52 -0700

    Hides Arrow Visual during Transition
    The 2 Arrow visuals are now hidden during level transition

--- Assets/Scripts/ArrowDash.cs
+++ Assets/Scripts/ArrowDash.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class ArrowDash : MonoBehaviour
 {
+    PlayerInput Player; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Player = GameObject.Find("Player").GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Player.CanControl == true)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
 }
--- Assets/Scripts/ArrowDashVisual.cs
+++ Assets/Scripts/ArrowDashVisual.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class ArrowDashVisual : MonoBehaviour
 {
+    PlayerInput Player;
     // Start is called before the first frame update
     void Start()
     {
-
+        Player = GameObject.Find("Player").GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Player.CanControl == true)
         {
-            GetComponent<SpriteRenderer>().enabled = true;
+            if (Input.GetMouseButtonDown(1))
+            {
+                GetComponent<SpriteRenderer>().enabled = true;
+            }
+            if (Input.GetMouseButtonUp(1))
+            {
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
         }
-        if (Input.GetMouseButtonUp(1))
+        else
         {
             GetComponent<SpriteRenderer>().enabled = false;
         }
