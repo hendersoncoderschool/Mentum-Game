c93f27538ce18ac0304d13351e14f7c7ee463f22
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2026-03-21 14:26:49 -0700

    Working Level Transition
    Level Transition has been finished

--- Assets/FadeTransition.cs
+++ Assets/FadeTransition.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
 
 public class FadeTransition : MonoBehaviour
 {
     GameObject Player;
     public bool fade = false;
-    private float fadeamount = 0;
+    private float fadeamount = 24;
     RectTransform rectTransform;
 
     // Start is called before the first frame update
@@ -15,6 +17,7 @@ public class FadeTransition : MonoBehaviour
     {
         rectTransform = GetComponent<RectTransform>();
         Player = GameObject.FindWithTag("Player");
+        fadeamount = 24;
     }
 
     // Update is called once per frame
@@ -22,8 +25,31 @@ public class FadeTransition : MonoBehaviour
     {
         if (fade == true)
         {
-            fadeamount += Time.deltaTime * 10 * (1 + (fadeamount * 0.3f));
-            rectTransform.localScale = new Vector3(24.688f, fadeamount, 1);
+            if (fadeamount < 24)
+            {
+                fadeamount += Time.deltaTime * 10 * (1 + (fadeamount * 0.3f));
+            }
+            else
+            {
+                fadeamount = 24;
+            }
+
+            if (fadeamount > 24)
+            {
+                SceneManager.LoadScene("Testing Grounds");
+            }
         }
+        else
+        {
+            if (fadeamount > 0)
+            {
+            fadeamount -= Time.deltaTime * 10 * (1 + (fadeamount * 0.3f));
+            }
+            else
+            {
+                fadeamount = 0;
+            }
+        }
+        rectTransform.localScale = new Vector3(24.688f, fadeamount, 1);
     }
 }
