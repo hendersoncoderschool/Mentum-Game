d147c62dba46e58ef7d66022eee225f1b57aa4ad
bowser-bash <garrisong2009@gmail.com>
2026-04-11 14:17:39 -0700

    Added Pausing
    You can now pause the game, though it is not fully finished

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    RectTransform rectTransform;
+    public bool Paused = false;
+    public float TimeScale = 1f;
+    public float Squish = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Paused = !Paused;
+        }
+        if (Paused == true)
+        {
+            Time.timeScale = 0;
+            if (Squish < 1)
+            {
+                //Squish += Time.deltaTime * 2 * (1 + (Squish * 0.05f));
+                Squish = Squish + 0.01f;
+                {
+                    if (Squish > 1)
+                    {
+                        Squish = 1;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Time.timeScale = TimeScale;
+            if (Squish > 0f)
+            {
+                //Squish -= Time.deltaTime * 2 * (1 + (Squish * 0.05f));
+                Squish -= 0.01f;
+                {
+                    if (Squish < 0)
+                    {
+                        Squish = 0;
+                    }
+                }
+            }
+        }
+        rectTransform.localScale = new Vector3(Squish, 1, 1);
+    }
+}
