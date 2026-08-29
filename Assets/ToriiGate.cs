c7d54265ecfc86279bdaa8e3dee553a6b7d636bf
bowser-bash <garrisong2009@gmail.com>
2026-08-29 15:01:38 -0700

    Working on Torii Gates
    Still working on the Torii Gates. Next commit they will 100% be working as intended, as I have an idea of what the function is, I just need to figure out what type of function it is.

--- Assets/ToriiGate.cs
+++ Assets/ToriiGate.cs
@@ -4,10 +4,8 @@ using UnityEngine;
 
 public class ToriiGate : MonoBehaviour
 {
-    private float maxSpeed = 30f;
-    private float falloff = 2f;
-    private float maxBoost = 20f;
-    //private BoxCollider2D ToriiTrigger;
+    private float maxSpeed = 500f;
+    private float maxBoost = 30f;
     AudioSource audioSource;
     public AudioClip sfx_gate;
     // Start is called before the first frame update
@@ -18,20 +16,25 @@ public class ToriiGate : MonoBehaviour
     }
 
     // Update is called once per frame
-    /*void Update()
+    public void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }*/
-    void OnTriggerEnter(Collider other)
-    {
-        audioSource.PlayOneShot(sfx_gate, 1);
-        Vector3 velocity = other.attachedRigidbody.velocity;
-        float speed = other.attachedRigidbody.velocity.magnitude;
-        Vector3 direction = speed > 0.01f ? velocity / speed : transform.forward;
-        float t = Mathf.Clamp01(speed / maxSpeed);
-        float gain = maxBoost + Mathf.Pow(1f - t, falloff);
+        if (other.CompareTag("Player"))
+        {
+            audioSource.PlayOneShot(sfx_gate, 1);
+            if (other.attachedRigidbody.velocity.magnitude >= maxSpeed) return;
+            Vector3 velocity = other.attachedRigidbody.velocity;
+            float speed = other.attachedRigidbody.velocity.magnitude;
+            Vector3 direction = speed > 0.01f ? velocity / speed : transform.forward;
+            float speedRatio = speed / maxSpeed;
+            float diminishingMult = 1f - Mathf.Log10(1f + (speedRatio * 9f));
+            diminishingMult = Mathf.Clamp01(diminishingMult);
+            other.attachedRigidbody.velocity = direction * (maxBoost * diminishingMult);
+            Debug.Log("Passed Torii Gate");
+        }
+    }
 
-        other.attachedRigidbody.velocity = direction * Mathf.Min(speed + gain, maxSpeed);
-        Debug.Log("Passed Torii Gate");
+    float CalculateBoost(Vector3 Velocity)
+    {
+
     }
 }
